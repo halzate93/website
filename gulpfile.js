bb5b9ebbbce2eeaf8cd4705f4b6c216ebc589a36
Héctor C. Alzate <halzate93@gmail.com>
2016-02-21 23:16:56 -0500

    Working on gulp build file

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
+const imagemin = require('gulp-imagemin')
+const pngquant = require('imagemin-pngquant')
+const jpegtran = require('imagemin-jpegtran')
+const optipng = require('imagemin-optipng')
+const gifsicle = require('imagemin-gifsicle')
+const minifyJs = require('gulp-uglify')
+const minifyCss = require('gulp-cssnano')
+const useref = require('gulp-useref')
+const gulpif = require('gulp-if')
+// const util = require('gulp-util')
+
+gulp.task('default', ['minify img', 'copy fonts', 'process'])
+
+gulp.task('minify img', function () {
+  gulp.src('img/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      optimizationLevel: 3,
+      interlaced: true,
+      use: [pngquant(), jpegtran(), optipng(), gifsicle()]
+    }))
+    .pipe(gulp.dest('dist/img/'))
+})
+
+gulp.task('process', function () {
+  gulp.src('*.html')
+    .pipe(useref())
+    .pipe(gulpif('*.css', minifyCss({processImport: false})))
+    .pipe(gulpif('*.js', minifyJs()))
+    .pipe(gulpif('*.html', htmlmin({collapseWhitespace: true})))
+    .pipe(gulp.dest('dist/'))
+})
+
+gulp.task('copy fonts', function () {
+  gulp.src(['css/fonts/*'], {})
+    .pipe(gulp.dest('dist/css/fonts/'))
+})
